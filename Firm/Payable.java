2df49b63bf71b6a29ab4210150158194563ee4e0
Benjamin Prud'homme <bgprudhomme@gmail.com>
2017-11-04 12:41:53 -0400

    Initial commit

---
+++ Firm/Payable.java
@@ -0,0 +1,7 @@
+public interface Payable {
+	//----------------------------------------------------------------
+	//  Derived classes must define the pay method for each type of
+	//  employee.
+	//----------------------------------------------------------------
+	public double pay();
+}

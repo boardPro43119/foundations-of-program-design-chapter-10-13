2080a7e2286a9ed6ef635307dc26dd6e6b1b30c8
Benjamin Prud'homme <bgprudhomme@gmail.com>
2018-01-20 11:49:34 -0500

    Improve recursion in RecursivePalindromeTestDriver

--- Chapter 12/RecursivePalindromeTestDriver.java
+++ Chapter 12/RecursivePalindromeTestDriver.java
@@ -2,63 +2,40 @@ import java.util.Scanner;
 
 public class RecursivePalindromeTestDriver {
 	
-	public static String test(String input){	
-		String cleanInput = input.toLowerCase().replaceAll("[^a-z]", "");
+	public static boolean test(String input){	
 		
-		int length = cleanInput.length();
-		
-		int firstHalfEnd;
-		int secondHalfStart;
-		
-		String secondHalfReversed = "";
-		
-		if(length%2 == 0){
-			firstHalfEnd = length/2 - 1;
-			secondHalfStart = length/2;
+		String cleanString = input.toLowerCase().replaceAll("[^a-z]", "");
+		int length = cleanString.length();
 			
-		}
-		else{
-			firstHalfEnd = length/2;
-			secondHalfStart = firstHalfEnd;
-		}	
-			
-		String firstHalf = cleanInput.substring(0, firstHalfEnd+1);
-		String secondHalf = cleanInput.substring(secondHalfStart, length);
-		
-		//debugging
-		System.out.println("First half: characters 0 to " + firstHalfEnd);
-		System.out.println("Second half: characters " + secondHalfStart + " to " + length);
-		System.out.println(firstHalf + "|" + secondHalf);
-
-		if(firstHalf.equalsIgnoreCase(reverseString(secondHalf))){
-			return "That string IS a palindrome.";
-		}
-		else{
-			return "That string is NOT a palindrome.";
-		}
-	}
-	
-	public static String reverseString(String str){ 
-	    String result = "";
-		
-		int length = str.length();
-		
 		if(length == 1){
-			result = str;
+			return true;
 		}
-		else{
-			result = str.charAt(length-1) + reverseString(str.substring(0, length-1));
+		else {
+			if(cleanString.charAt(0) == cleanString.charAt(length-1)){
+				if(length == 2){
+					return true;
+				}
+				else {
+					return test(cleanString.substring(1, length-1));
+				}
+			}
 		}
+		return false;
 		
-		return result;
- 	}
+		
+	}
 	
 	public static void main(String[] args){
 		String another = "y";
 		Scanner in = new Scanner(System.in);
 		while(another.equalsIgnoreCase("y")){
 			System.out.print("Enter a potential palindrome: ");
-			System.out.println(test(in.nextLine()));
+			if(test(in.nextLine())){
+				System.out.println("That string IS a palindrome.");
+			}
+			else {
+				System.out.println("That string is NOT a palindrome.");
+			}
 			
 			System.out.print("Test another palindrome (y/n)? ");
 			another = in.nextLine();

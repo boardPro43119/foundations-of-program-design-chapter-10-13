9c82eca706089f0fe3cdf0b1421bf7fe21089a92
Benjamin Prud'homme <bgprudhomme@gmail.com>
2018-02-03 12:41:40 -0500

    Initial commit for NonAttackingQueens (WIP)

---
+++ Chapter 12/NonAttackingQueens.java
@@ -0,0 +1,39 @@
+public class NonAttackingQueens{
+	public int[] board = {
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1},
+							{1, 1, 1, 1, 1, 1 ,1, 1}
+						};
+
+	public final int QUEEN = 7;
+	public final int ATTACKED = 0;
+
+	public void solve(){
+		placeQueens(0);
+	}
+
+	private void placeQueens(int startingRow){
+
+	}
+
+	private boolean rowEmpty(){
+
+	}
+
+	private boolean columnEmpty(){
+		
+	}
+
+	private boolean diagonalsEmpty(){
+		
+	}
+
+	public String toString(){
+
+	}
+}
---
+++ Chapter 12/NonAttackingQueensDriver.java
@@ -0,0 +1,16 @@
+public class NonAttackingQueensDriver {
+	public static void main(String[] args){
+		// initalize the empty board (two dimensional array)
+		
+
+		// place each queen by rank
+
+			// upon placing each, block off the squares that it attacks
+
+			// for each remaining queen, first try to place it on the first available
+			// unattacked square going from the top left corner
+
+				// if at some point we can't place the next queen, the chain of recursion ends
+
+	}
+}
